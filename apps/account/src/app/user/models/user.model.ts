8d45c18979c14442a3e162d8066d45b5bd19f8da
Vitalii Zgura <Vitalii.Zgura@lantec.ua>
2023-08-11 00:00:50 +0200

    -

---
+++ apps/account/src/app/user/models/user.model.ts
@@ -0,0 +1,25 @@
+import { IUser, UserRole } from '@hp/interfaces';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document implements IUser {
+  @Prop()
+  displayName?: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
+
+  @Prop({
+    required: true,
+    enum: UserRole,
+    type: String,
+    default: UserRole.Student,
+  })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
---
+++ apps/account/src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './models/user.model';
+import { UserRepository } from './repositories/user.repositiry';
+import { UserController } from './user.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserRepository],
+  exports:[UserRepository]
+})
+export class UserModule {}
